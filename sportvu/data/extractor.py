23a64d94041adcaacc3db283f81ed7909fad0eb9
Jackson Wang <kcjacksonwang@gmail.com>
2017-04-07 12:03:50 -0400

    bug fix: checking malformed event in extract_raw

--- sportvu/data/extractor.py
+++ sportvu/data/extractor.py
@@ -5,13 +5,12 @@ import os
 from sportvu import data
 import numpy as np
 import yaml
-from utils import pictorialize_team, pictorialize_fast,make_3teams_11players
+from utils import pictorialize_team, pictorialize_fast, make_3teams_11players
 game_dir = data.constant.game_dir
 
 
 class ExtractorException(Exception):
     pass
-
 
 
 class BaseExtractor:
@@ -50,7 +49,12 @@ class BaseExtractor:
                 else:  # defense
                     defense[def_id].append([player.x, player.y])
                     def_id += 1
-        if not ( len(ball[0]) == len(offense[0]) and len(defense[0]) == len(offense[0])):
+        if not ((len(np.array(ball).shape) == 3
+                 and len(np.array(offense).shape) == 3
+                 and len(np.array(defense).shape) == 3)
+                and
+                (np.array(ball).shape[1] == np.array(offense).shape[1]
+                 and np.array(offense).shape[1] == np.array(defense).shape[1])):
             raise ExtractorException()
         return [ball, offense, defense]
 
@@ -65,69 +69,71 @@ class BaseExtractor:
             for team in x:
                 for player in team:
                     try:
-                        player=np.array(player) + jit
-                    except ValueError: # bad sequence where not all players have the same number of moments
+                        player = np.array(player) + jit
+                    except ValueError:  # bad sequence where not all players have the same number of moments
                         raise ExtractorException()
 
         for play_sequence in x:
             try:
-                team_matrix=np.concatenate(play_sequence, 1)
+                team_matrix = np.concatenate(play_sequence, 1)
             except ValueError:
                 raise ExtractorException()
 
-            tm=pictorialize_team(team_matrix, sample_rate=self.config['sample_rate'],
-                                   Y_RANGE=self.config['Y_RANGE'], X_RANGE=self.config['X_RANGE'],
+            tm = pictorialize_team(team_matrix, sample_rate=self.config['sample_rate'],
+                                   Y_RANGE=self.config[
+                                       'Y_RANGE'], X_RANGE=self.config['X_RANGE'],
                                    radius=self.config['radius'])
-                
-                
+
             ctxy.append(tm)
         ctxy = np.array(ctxy)
-        if len(ctxy.shape) == 1: ## different teams have different length
+        if len(ctxy.shape) == 1:  # different teams have different length
             raise ExtractorException()
-        ## compress the time dimension
+        # compress the time dimension
         if 'video' in self.config and self.config['video']:
-            if self.augment and self.config['d0flip'] and np.random.rand>.5:
-                ctxy = ctxy[:,:,::-1]
-            if self.augment and self.config['d1flip'] and np.random.rand>.5:
-                ctxy = ctxy[:,:,:,::-1]
+            if self.augment and self.config['d0flip'] and np.random.rand > .5:
+                ctxy = ctxy[:, :, ::-1]
+            if self.augment and self.config['d1flip'] and np.random.rand > .5:
+                ctxy = ctxy[:, :, :, ::-1]
             return ctxy
         else:
             cxy = ctxy.sum(1)
-            cxy[cxy>1] = 1
-            if self.augment and self.config['d0flip'] and np.random.rand>.5:
-                cxy = cxy[:,::-1]
-            if self.augment and self.config['d1flip'] and np.random.rand>.5:
-                cxy = cxy[:,:,::-1]
+            cxy[cxy > 1] = 1
+            if self.augment and self.config['d0flip'] and np.random.rand > .5:
+                cxy = cxy[:, ::-1]
+            if self.augment and self.config['d1flip'] and np.random.rand > .5:
+                cxy = cxy[:, :, ::-1]
             return cxy
 
     def extract_batch(self, events_arr):
-        sequences = np.array([make_3teams_11players(self.extract_raw(e)) for e in events_arr])
+        sequences = np.array([make_3teams_11players(
+            self.extract_raw(e)) for e in events_arr])
         if self.augment and np.sum(self.config['jitter']) > 0:
             d0_jit = (np.random.rand() * 2 - 1) * self.config['jitter'][0]
             d1_jit = (np.random.rand() * 2 - 1) * self.config['jitter'][1]
-            try: ### hacky: can delete after -- temporary for malformed data (i.e. missing player)
-                sequences[:,:,:,0] += d0_jit    
+            # hacky: can delete after -- temporary for malformed data (i.e.
+            # missing player)
+            try:
+                sequences[:, :, :, 0] += d0_jit
             except:
-                raise ExtractorException() 
-            sequences[:,:,:,1] += d1_jit    
-        ## 
+                raise ExtractorException()
+            sequences[:, :, :, 1] += d1_jit
+        ##
         bctxy = pictorialize_fast(sequences)
-        ## compress the time dimension
+        # compress the time dimension
         if 'video' in self.config and self.config['video']:
-            if self.augment and self.config['d0flip'] and np.random.rand>.5:
-                bctxy = bctxy[:,:,:,::-1]
-            if self.augment and self.config['d1flip'] and np.random.rand>.5:
-                bctxy = bctxy[:,:,:,:,::-1]
+            if self.augment and self.config['d0flip'] and np.random.rand > .5:
+                bctxy = bctxy[:, :, :, ::-1]
+            if self.augment and self.config['d1flip'] and np.random.rand > .5:
+                bctxy = bctxy[:, :, :, :, ::-1]
             return bctxy
         else:
             bcxy = bctxy.sum(2)
-            bcxy[bcxy>1] = 1
-            if self.augment and self.config['d0flip'] and np.random.rand>.5:
-                bcxy = bcxy[:,:,::-1]
-            if self.augment and self.config['d1flip'] and np.random.rand>.5:
-                bcxy = bcxy[:,:,:,::-1]
+            bcxy[bcxy > 1] = 1
+            if self.augment and self.config['d0flip'] and np.random.rand > .5:
+                bcxy = bcxy[:, :, ::-1]
+            if self.augment and self.config['d1flip'] and np.random.rand > .5:
+                bcxy = bcxy[:, :, :, ::-1]
             return bcxy
-
 
 
 """
@@ -153,29 +159,28 @@ if __name__ == '__main__':
     from loader import BaseLoader
     ##
     f_config = 'config/train_rev0.yaml'
-    dataset=BaseDataset(f_config, 0)
-    extractor=BaseExtractor(f_config)
-    loader=BaseLoader(dataset, extractor, 35, fraction_positive=0)
+    dataset = BaseDataset(f_config, 0)
+    extractor = BaseExtractor(f_config)
+    loader = BaseLoader(dataset, extractor, 35, fraction_positive=0)
     print ('testing next_batch')
-    batch=loader.next_batch(extract=False)
-    for eind,  event in enumerate(batch[0]):
-        event.show('/home/wangkua1/Pictures/vis/%i.mp4'%eind)
+    batch = loader.next_batch(extract=False)
+    for eind, event in enumerate(batch[0]):
+        event.show('/home/wangkua1/Pictures/vis/%i.mp4' % eind)
 
-    ## visualize model input
+    # visualize model input
     # import  matplotlib.pyplot as plt
     # plt.ion()
     # for x in batch[0]:
     #     img = np.rollaxis(x, 0, 3)
     #     plt.imshow(img)
     #     raw_input()
-    
 
     # ## concurrent
     # import sys
     # sys.path.append('/home/wangkua1/toolboxes/resnet')
     # from resnet.utils.concurrent_batch_iter import ConcurrentBatchIterator
     # from tqdm import tqdm
-    
+
     # print ("compare loading latency")
     # Q_size = 100
     # N_thread = 32
@@ -186,4 +191,3 @@ if __name__ == '__main__':
 
     # for i in tqdm(xrange(N), desc='single thread'):
     #     b = loader.next()
-
--- sportvu/make_sequence_data.py
+++ sportvu/make_sequence_data.py
@@ -20,7 +20,7 @@ import os
 from docopt import docopt
 import yaml
 import numpy as np
-
+from sportvu.data.utils import make_3teams_11players, pictorialize_fast
 f_data_config = 'data/config/train_rev0.yaml'
 fold_index = 0
 dataset = BaseDataset(f_data_config, fold_index=fold_index)
@@ -29,15 +29,30 @@ loader = BaseLoader(dataset, extractor, 32, fraction_positive=.5)
 
 
 
-batch = loader.next_batch(False)
-sequences = np.array([make_3teams_11players(extractor.extract_raw(e)) for e in batch[0]])
-model_x = pictorialize_fast(sequences)
+# batch = loader.next_batch(False)
+# sequences = np.array([make_3teams_11players(extractor.extract_raw(e)) for e in batch[0]])
+# model_x = pictorialize_fast(sequences)
 # for i in tqdm(range(10)):
 #     sequences = np.array([make_3teams_11players(extractor.extract_raw(e)) for e in batch[0]])
 #     model_x = pictorialize_fast(sequences)
 # for i in tqdm(range(10)):
 #     for j in xrange(len(batch[0])):
 #         a = extractor.extract(batch[0][j])
+
+
+N_iter = 10
+for i in tqdm(range(N_iter)):
+	batch = loader.next_batch(False)
+
+for i in tqdm(range(N_iter)):
+	[extractor.extract_raw(e) for e in batch[0]]
+
+for i in tqdm(range(N_iter)):
+	sequences = np.array([make_3teams_11players(extractor.extract_raw(e)) for e in batch[0]])
+
+for i in tqdm(range(N_iter)):
+	model_x = pictorialize_fast(sequences)
+
 
 
 
