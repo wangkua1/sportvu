2e9c2eddd3fdd74a1740a0302052a06ad592d4df
ethanfetaya@gmail.com <ethanfetaya@gmail.com>
2017-08-21 16:21:47 -0400

    Gauss loss bugs fixed

--- sportvu/loss/full_gauss_NLL.py
+++ sportvu/loss/full_gauss_NLL.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
+from tensorflow.python.ops.distributions.util import fill_lower_triangular
 
 
-class full_gauss_NLL(object):
+class FullGaussNLL(object):
     def __init__(self):
-        super(full_gauss_NLL, self).__init__()
+        super(FullGaussNLL, self).__init__()
 
     def build_tf_loss(self, pred, y_, eps=1e-5):
         """
@@ -13,13 +14,13 @@ class full_gauss_NLL(object):
         pred shape (N, T, k+k*(k-1)/2)
 
         """
-        pred_dim = 22
+        pred_dim = y_.get_shape()[-1].value()
         mean, R_trans = tf.split(pred, [pred_dim, pred_dim * (pred_dim + 1) / 2],
                                  axis=-1)  # Sigma_inv = R^TR cholasky upper decomp
         R_trans = fill_lower_triangular(tf.nn.softplus(R_trans) + eps)
 
-        diff_shape = [s.value for s in gt.shape].append(1)
-        diff = tf.reshape(mean - gt, diff_shape)
+        diff_shape = [s.value for s in y_.shape].append(1)
+        diff = tf.reshape(mean - y_, diff_shape)
         diff_loss = tf.reduce_mean(tf.pow(tf.matmul(R_trans, diff), 2))
         normalizer_loss = tf.reduce_mean(tf.log(tf.matrix_diag_part(R_trans)))
         return diff_loss + normalizer_loss  # +CONST
