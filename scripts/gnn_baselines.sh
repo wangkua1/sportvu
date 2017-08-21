fb7aeb79e07ac809068a1bdd7c54d31c57c659b2
Jackson Wang <kcjacksonwang@gmail.com>
2017-08-21 17:49:50 -0400

    non-code: script and configs for baselines with RMSE

---
+++ scripts/gnn_baselines.sh
@@ -0,0 +1,8 @@
+python train-seq2seq.py 0 data/config/rnn_location_single_enc.yaml model/config/rnn_location_single.yaml RMSEPerPlayer
+python train-seq2seq.py 0 data/config/rnn_velocity_single_enc.yaml model/config/rnn_velocity_single.yaml RMSEPerPlayer
+python train-seq2seq.py 0 data/config/rnn_location_single_enc.yaml model/config/rnn_location_single_enc.yaml RMSEPerPlayer
+python train-seq2seq.py 0 data/config/rnn_velocity_single_enc.yaml model/config/rnn_velocity_single_enc.yaml RMSEPerPlayer
+python train-seq2seq.py 0 data/config/rnn_location_full_enc.yaml model/config/rnn_location_full.yaml RMSEPerPlayer
+python train-seq2seq.py 0 data/config/rnn_velocity_full_enc.yaml model/config/rnn_velocity_full.yaml RMSEPerPlayer
+python train-seq2seq.py 0 data/config/rnn_location_full_enc.yaml model/config/rnn_location_full_enc.yaml RMSEPerPlayer
+python train-seq2seq.py 0 data/config/rnn_velocity_full_enc.yaml model/config/rnn_velocity_full_enc.yaml RMSEPerPlayer
